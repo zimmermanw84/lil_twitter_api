640fbc2b13444a7e4b1a041652a8ee43a7f58d3f
Walter Zimmerman <zimmermanw84@gmail.com>
2015-03-24 09:58:45 -0700

    base tweet functionallity implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+  def handle_options_request
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+  end
 end
